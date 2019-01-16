2ba13fc5e16586880d41e0855e90714813ecb8ce
Trevor Eyre <trevoreyre@gmail.com>
2019-01-16 06:10:51 -0700

    First pass at rollup build for Vue component

--- src/vue/index.js
+++ src/vue/index.js
@@ -1,1 +1,28 @@
-export { default } from './Autocomplete.vue'
+import Autocomplete from './Autocomplete.vue'
+
+function install (Vue) {
+  if (install.installed) {
+    return
+  }
+  install.installed = true
+  Vue.component('Autocomplete', Autocomplete)
+}
+
+const plugin = { install }
+
+// Auto install if Vue is found
+let GlobalVue
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin)
+}
+
+// Inject install function into component. Allows component to be registered via
+// Vue.use() as well as Vue.component()
+Autocomplete.install = install
+
+export default Autocomplete
