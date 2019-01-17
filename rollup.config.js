2a52fc9c6f8c683fefc35b859560a381305f839f
Trevor Eyre <trevoreyre@gmail.com>
2019-01-17 08:32:49 -0700

    Updated rollup config. Deleted rollup configs in individual packages.

--- rollup.config.js
+++ rollup.config.js
@@ -3,99 +3,61 @@ import babel from 'rollup-plugin-babel'
 import uglify from 'rollup-plugin-uglify-es'
 import vue from 'rollup-plugin-vue'
 
-export default [{
-  input: 'packages/autocomplete/index.js',
-  output: [{
-    file: 'packages/autocomplete/dist/autocomplete.cjs.js',
-    format: 'cjs'
+// Creates three bundles, a CommonJS bundle for Node, an ES modules bundle for use in
+// other bundlers such as Webpack or Rollup, and an IIFE bundle for use in the browser
+// in a <script> tag. All three bundles are transpiled to ES5 (with exception of
+// the import/export statements in the ES bundle).
+const createConfigs = ({ root = '', plugins = [] }) => {
+  console.log('createConfigs', root, plugins);
+  return [{
+    input: `${root}/index.js`,
+    output: [{
+      file: `${root}/dist/autocomplete.cjs.js`,
+      format: 'cjs'
+    }, {
+      file: `${root}/dist/autocomplete.es.js`,
+      format: 'es'
+    }],
+    plugins: [
+      ...plugins,
+      babel({
+        exclude: 'node_modules/**'
+      })
+    ]
   }, {
-    file: 'packages/autocomplete/dist/autocomplete.es.js',
-    format: 'es'
-  }],
-  plugins: [
-    babel({
-      exclude: 'node_modules/**'
-    })
-  ]
-}, {
-  input: 'packages/autocomplete/index.js',
-  output: {
-    name: 'Autocomplete',
-    file: 'packages/autocomplete/dist/autocomplete.min.js',
-    format: 'iife'
-  },
-  plugins: [
-    babel({
-      exclude: 'node_modules/**'
-    }),
-    uglify()
-  ]
-}, {
-  input: 'packages/autocomplete-js/index.js',
-  output: [{
-    file: 'packages/autocomplete-js/dist/autocomplete.cjs.js',
-    format: 'cjs'
-  }, {
-    file: 'packages/autocomplete-js/dist/autocomplete.es.js',
-    format: 'es'
-  }],
-  plugins: [
-    babel({
-      exclude: 'node_modules/**'
-    })
-  ]
-}, {
-  input: 'packages/autocomplete-js/index.js',
-  output: {
-    name: 'Autocomplete',
-    file: 'packages/autocomplete-js/dist/autocomplete.min.js',
-    format: 'iife'
-  },
-  plugins: [
-    babel({
-      exclude: 'node_modules/**'
-    }),
-    uglify()
-  ]
-}, {
-  input: 'packages/autocomplete-vue/index.js',
-  output: [{
-    file: 'packages/autocomplete-vue/dist/autocomplete.cjs.js',
-    format: 'cjs'
-  }, {
-    file: 'packages/autocomplete-vue/dist/autocomplete.es.js',
-    format: 'es'
-  }],
-  plugins: [
-    vue({
-      css: false,
-      compileTemplate: true,
-      template: {
-        isProduction: true
-      }
-    }),
-    babel({
-      exclude: 'node_modules/**'
-    })
-  ]
-}, {
-  input: 'packages/autocomplete-vue/index.js',
-  output: {
-    name: 'Autocomplete',
-    file: 'packages/autocomplete-vue/dist/autocomplete.min.js',
-    format: 'iife'
-  },
-  plugins: [
-    vue({
-      css: false,
-      compileTemplate: true,
-      template: {
-        isProduction: true
-      }
-    }),
-    babel({
-      exclude: 'node_modules/**'
-    }),
-    uglify()
-  ]
-}]
+    input: `${root}/index.js`,
+    output: {
+      name: 'Autocomplete',
+      file: `${root}/dist/autocomplete.min.js`,
+      format: 'iife'
+    },
+    plugins: [
+      ...plugins,
+      babel({
+        exclude: 'node_modules/**'
+      }),
+      uglify()
+    ]
+  }]
+}
+
+export default [
+  ...createConfigs({
+    root: 'packages/autocomplete'
+  }),
+  ...createConfigs({
+    root: 'packages/autocomplete-js'
+  }),
+  ...createConfigs({
+    root: 'packages/autocomplete-vue',
+    plugins: [
+      vue({
+        css: false,
+        compileTemplate: true,
+        template: {
+          isProduction: true
+        }
+      })
+    ]
+  })
+]
