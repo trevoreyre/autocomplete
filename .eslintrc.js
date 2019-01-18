dcca6b5a5647a36d448a6fe66f89311c1c149cf1
Trevor Eyre <trevoreyre@gmail.com>
2019-01-18 14:53:04 +0000

    Try to get prettier working on .vue files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,24 @@
 module.exports = {
-    extends: [
-      'plugin:vue/recommended',
-      'plugin:prettier/recommended'
-    ],
-    rules: {
-      'prettier/prettier': [
-        'warn',
-        {
-          semi: false,
-          singleQuote: true,
-          trailingComma: 'es5'
-        }
-      ]
-    },
-    parserOptions: {
-      parser: "babel-eslint"
-    }
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/recommended',
+    // 'plugin:prettier/recommended',
+    '@vue/prettier',
+    'eslint:recommended'
+  ],
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        semi: false,
+        singleQuote: true,
+        trailingComma: 'es5'
+      }
+    ]
+  },
+  parserOptions: {
+    parser: "babel-eslint"
   }
+}
