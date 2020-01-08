d2c1153e000a55ac669c2b5858b51b1546bea3ac
Justin Souders <justin.souders@grainbridge.com>
2020-01-08 10:54:43 -0600

    Adding debounce function to utils

---
+++ packages/autocomplete/util/debounce.js
@@ -0,0 +1,29 @@
+// Credit David Walsh (https://davidwalsh.name/javascript-debounce-function)
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+const debounce = (func, wait, immediate) => {
+  var timeout
+
+  return function executedFunction() {
+    var context = this
+    var args = arguments
+
+    var later = function() {
+      timeout = null
+      if (!immediate) func.apply(context, args)
+    }
+
+    var callNow = immediate && !timeout
+
+    clearTimeout(timeout)
+
+    timeout = setTimeout(later, wait)
+
+    if (callNow) func.apply(context, args)
+  }
+}
+
+export default debounce
