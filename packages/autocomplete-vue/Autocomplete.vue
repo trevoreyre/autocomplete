8417bdbb7c0767d45c39d376a1dc0195c4518c4e
Michal Štván <michalstvan@gmail.com>
2021-04-19 15:26:05 +0200

    Emit selected value
    It's very useful to have an option to listen for selected item.

--- packages/autocomplete-vue/Autocomplete.vue
+++ packages/autocomplete-vue/Autocomplete.vue
@@ -198,7 +198,8 @@ export default {
 
   methods: {
     setValue(result) {
-      this.value = result ? this.getResultValue(result) : ''
+      this.value = result ? this.getResultValue(result) : '' 
+      this.$emit('selected', result)
     },
 
     handleUpdate(results, selectedIndex) {
