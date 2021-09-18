3f832f876fccef6b69de47d7ebfa60553ccfa46a
YangLei <xcchcaptain@126.com>
2021-09-18 16:38:21 +0800

    a story for japanese

--- packages/autocomplete-vue/Autocomplete.stories.js
+++ packages/autocomplete-vue/Autocomplete.stories.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import { storiesOf } from '@storybook/vue'
 import { action } from '@storybook/addon-actions'
 import Autocomplete from './Autocomplete.vue'
 
@@ -46,288 +47,255 @@ const searchWikipedia = input =>
       })
   })
 
-export default {
-  title: 'Autocomplete Vue',
-  component: Autocomplete,
-}
-
-export const Basic = () => ({
-  template: `
-    <Autocomplete
-      aria-label="Search for a country"
-      placeholder="Search for a country"
-      :search="search"
-    />
-  `,
-  methods: {
-    search,
-  },
-})
-
-export const DefaultResults = () => ({
-  template: `
-    <Autocomplete
-      aria-label="Search for a country"
-      placeholder="Search for a country"
-      :search="search"
-    />
-  `,
-  methods: {
-    search(input) {
-      if (input.length < 1) {
-        return ['Canada', 'Mexico', 'United Kingdom', 'Russia']
-      }
-      return countries.filter(country => {
-        return country.toLowerCase().startsWith(input.toLowerCase())
-      })
+storiesOf('Autocomplete Vue', module)
+  .add('Default', () => ({
+    components: { Autocomplete },
+    template: `
+      <Autocomplete
+        aria-label="Search for a country"
+        placeholder="Search for a country"
+        :search="search"
+      />
+    `,
+    methods: {
+      search,
     },
-  },
-})
-
-export const AdvancedSearch = () => ({
-  template: `
-    <Autocomplete
-      aria-label="Search Wikipedia"
-      placeholder="Search Wikipedia"
-      :search="search"
-      :get-result-value="getResultValue"
-      @submit="onSubmit"
-    />
-  `,
-  methods: {
-    search(input) {
-      return searchWikipedia(input)
+  }))
+  .add('Default results', () => ({
+    components: { Autocomplete },
+    template: `
+      <Autocomplete
+        aria-label="Search for a country"
+        placeholder="Search for a country"
+        :search="search"
+      />
+    `,
+    methods: {
+      search(input) {
+        if (input.length < 1) {
+          return ['Canada', 'Mexico', 'United Kingdom', 'Russia']
+        }
+        return countries.filter(country => {
+          return country.toLowerCase().startsWith(input.toLowerCase())
+        })
+      },
     },
-    getResultValue(result) {
-      return result.title
-    },
-    onSubmit(result) {
-      window.open(`${wikiUrl}/wiki/${encodeURI(result.title)}`)
-    },
-  },
-})
-
-export const DebouncedSearch = () => ({
-  template: `
-    <div>
-      <p>Search count - {{ searchCount }}</p>
+  }))
+  .add('Advanced search', () => ({
+    components: { Autocomplete },
+    template: `
       <Autocomplete
         aria-label="Search Wikipedia"
         placeholder="Search Wikipedia"
         :search="search"
         :get-result-value="getResultValue"
-        :debounce-time="500"
         @submit="onSubmit"
       />
-    </div>
-  `,
-  data() {
-    return {
-      searchCount: 0,
-    }
-  },
-  methods: {
-    search(input) {
-      this.searchCount += 1
-      return searchWikipedia(input)
+    `,
+    methods: {
+      search(input) {
+        return searchWikipedia(input)
+      },
+      getResultValue(result) {
+        return result.title
+      },
+      onSubmit(result) {
+        window.open(`${wikiUrl}/wiki/${encodeURI(result.title)}`)
+      },
     },
-    getResultValue(result) {
-      return result.title
+  }))
+  .add('Submit event', () => ({
+    components: { Autocomplete },
+    template: `
+      <Autocomplete
+        aria-label="Search for a country"
+        placeholder="Search for a country"
+        :search="search"
+        @submit="onSubmit"
+      />
+    `,
+    methods: {
+      search,
+      onSubmit(result) {
+        alert(`You selected ${result}`)
+      },
     },
-    onSubmit(result) {
-      window.open(`${wikiUrl}/wiki/${encodeURI(result.title)}`)
+  }))
+  .add('Custom class', () => ({
+    components: { Autocomplete },
+    template: `
+      <Autocomplete
+        aria-label="Search for a country"
+        placeholder="Search for a country"
+        :search="search"
+        base-class="search"
+      />
+    `,
+    methods: {
+      search,
     },
-  },
-})
-
-export const SubmitEvent = () => ({
-  template: `
-    <Autocomplete
-      aria-label="Search for a country"
-      placeholder="Search for a country"
-      :search="search"
-      @submit="onSubmit"
-    />
-  `,
-  methods: {
-    search,
-    onSubmit(result) {
-      alert(`You selected ${result}`)
+  }))
+  .add('Custom events', () => ({
+    components: { Autocomplete },
+    template: `
+      <Autocomplete
+        aria-label="Search for a country"
+        placeholder="Search for a country"
+        :search="search"
+        @input="handleInput"
+        @keyup="handleKeyup"
+      />
+    `,
+    methods: {
+      search,
+      handleInput(event) {
+        return action('input')(event)
+      },
+      handleKeyup(event) {
+        return action('keyup')(event)
+      },
     },
-  },
-})
-
-export const CustomClass = () => ({
-  template: `
-    <Autocomplete
-      aria-label="Search for a country"
-      placeholder="Search for a country"
-      :search="search"
-      base-class="search"
-    />
-  `,
-  methods: {
-    search,
-  },
-})
-
-export const CustomEvents = () => ({
-  template: `
-    <Autocomplete
-      aria-label="Search for a country"
-      placeholder="Search for a country"
-      :search="search"
-      @input="handleInput"
-      @keyup="handleKeyup"
-    />
-  `,
-  methods: {
-    search,
-    handleInput(event) {
-      return action('input')(event)
+  }))
+  .add('Auto select', () => ({
+    components: { Autocomplete },
+    template: `
+      <Autocomplete
+        aria-label="Search for a country"
+        placeholder="Search for a country"
+        :search="search"
+        auto-select
+      />
+    `,
+    methods: {
+      search,
     },
-    handleKeyup(event) {
-      return action('keyup')(event)
+  }))
+  .add('Default value', () => ({
+    components: { Autocomplete },
+    template: `
+      <Autocomplete
+        aria-label="Search for a country"
+        placeholder="Search for a country"
+        :search="search"
+        default-value="United Kingdom"
+      />
+    `,
+    methods: {
+      search,
     },
-  },
-})
-
-export const AutoSelect = () => ({
-  template: `
-    <Autocomplete
-      aria-label="Search for a country"
-      placeholder="Search for a country"
-      :search="search"
-      auto-select
-    />
-  `,
-  methods: {
-    search,
-  },
-})
-
-export const DefaultValue = () => ({
-  template: `
-    <Autocomplete
-      aria-label="Search for a country"
-      placeholder="Search for a country"
-      :search="search"
-      default-value="United Kingdom"
-    />
-  `,
-  methods: {
-    search,
-  },
-})
-
-export const ResultSlot = () => ({
-  template: `
-    <Autocomplete
-      aria-label="Search Wikipedia"
-      placeholder="Search Wikipedia"
-      :search="search"
-      :get-result-value="getResultValue"
-      @submit="onSubmit"
-    >
-      <template #result="{ result, props }">
-        <li
-          v-bind="props"
-          class="autocomplete-result wiki-result"
-        >
-          <div class="wiki-title">
-            {{ result.title }}
-          </div>
-          <div class="wiki-snippet" v-html="result.snippet" />
-        </li>
-      </template>
-    </Autocomplete>
-  `,
-  methods: {
-    search(input) {
-      return searchWikipedia(input)
-    },
-    getResultValue(result) {
-      return result.title
-    },
-    onSubmit(result) {
-      window.open(`${wikiUrl}/wiki/${encodeURI(result.title)}`)
-    },
-  },
-})
-
-export const DefaultSlotFullControl = () => ({
-  components: { CustomInput },
-  template: `
-    <Autocomplete
-      placeholder="Search for a country"
-      aria-label="Search for a country"
-      :search="search"
-    >
-      <template
-        #default="{
-          rootProps,
-          inputProps,
-          inputListeners,
-          resultListProps,
-          resultListListeners,
-          results,
-          resultProps
-        }"
+  }))
+  .add('Result slot', () => ({
+    components: { Autocomplete },
+    template: `
+      <Autocomplete
+        aria-label="Search Wikipedia"
+        placeholder="Search Wikipedia"
+        :search="search"
+        :get-result-value="getResultValue"
+        @submit="onSubmit"
       >
-        <div v-bind="rootProps">
-          <CustomInput
-            v-bind="inputProps"
-            v-on="inputListeners"
-            :class="[
-              'autocomplete-input',
-              { 'autocomplete-input-no-results': noResults },
-              { 'autocomplete-input-focused': focused }
-            ]"
-            @focus="handleFocus"
-            @blur="handleBlur"
-          />
-          <ul
-            v-if="noResults"
-            class="autocomplete-result-list"
-            style="position: absolute; z-index: 1; width: 100%; top: 100%;"
+        <template #result="{ result, props }">
+          <li
+            v-bind="props"
+            class="autocomplete-result wiki-result"
           >
-            <li class="autocomplete-result">
-              No results found
-            </li>
-          </ul>
-          <ul v-bind="resultListProps" v-on="resultListListeners">
-            <li
-              v-for="(result, index) in results"
-              :key="resultProps[index].id"
-              v-bind="resultProps[index]"
+            <div class="wiki-title">
+              {{ result.title }}
+            </div>
+            <div class="wiki-snippet" v-html="result.snippet" />
+          </li>
+        </template>
+      </Autocomplete>
+    `,
+    methods: {
+      search(input) {
+        return searchWikipedia(input)
+      },
+      getResultValue(result) {
+        return result.title
+      },
+      onSubmit(result) {
+        window.open(`${wikiUrl}/wiki/${encodeURI(result.title)}`)
+      },
+    },
+  }))
+  .add('Default slot (full control)', () => ({
+    components: { Autocomplete, CustomInput },
+    template: `
+      <Autocomplete
+        placeholder="Search for a country"
+        aria-label="Search for a country"
+        :search="search"
+      >
+        <template
+          #default="{
+            rootProps,
+            inputProps,
+            inputListeners,
+            resultListProps,
+            resultListListeners,
+            results,
+            resultProps
+          }"
+        >
+          <div v-bind="rootProps">
+            <CustomInput
+              v-bind="inputProps"
+              v-on="inputListeners"
+              :class="[
+                'autocomplete-input',
+                { 'autocomplete-input-no-results': noResults },
+                { 'autocomplete-input-focused': focused }
+              ]"
+              @focus="handleFocus"
+              @blur="handleBlur"
+            />
+            <ul
+              v-if="noResults"
+              class="autocomplete-result-list"
+              style="position: absolute; z-index: 1; width: 100%; top: 100%;"
             >
-              {{ result }}
-            </li>
-          </ul>
-        </div>
-      </template>
-    </Autocomplete>
-  `,
-  data() {
-    return {
-      focused: false,
-      value: '',
-      results: [],
-    }
-  },
-  computed: {
-    noResults() {
-      return this.value && this.results.length === 0
+              <li class="autocomplete-result">
+                No results found
+              </li>
+            </ul>
+            <ul v-bind="resultListProps" v-on="resultListListeners">
+              <li
+                v-for="(result, index) in results"
+                :key="resultProps[index].id"
+                v-bind="resultProps[index]"
+              >
+                {{ result }}
+              </li>
+            </ul>
+          </div>
+        </template>
+      </Autocomplete>
+    `,
+    data() {
+      return {
+        focused: false,
+        value: '',
+        results: [],
+      }
     },
-  },
-  methods: {
-    search(input) {
-      this.value = input
-      this.results = search(input)
-      return this.results
+    computed: {
+      noResults() {
+        return this.value && this.results.length === 0
+      },
     },
-    handleFocus() {
-      this.focused = true
+    methods: {
+      search(input) {
+        this.value = input
+        this.results = search(input)
+        return this.results
+      },
+      handleFocus() {
+        this.focused = true
+      },
+      handleBlur() {
+        this.focused = false
+      },
     },
   }))
   .add('Try some chinese', () => ({
@@ -370,13 +338,6 @@ export const DefaultSlotFullControl = () => ({
       },
     },
   }))
-    handleBlur() {
-      this.focused = false
-    },
-  },
-})
-DefaultSlotFullControl.story = { name: 'Default Slot (Full Control)' }
-
 const countries = [
   'Afghanistan',
   'Albania',
@@ -585,5 +546,5 @@ const foods = [
   '四川麻辣烫',
   '湖南木桶饭',
 ]
-// I don't know what this world means, use "aaa" tigger
+// I don't know what thess world means, use "aaa" tigger
 const sushis = ['きょう', 'あす', 'サクラ', 'やま', '亜亜']
